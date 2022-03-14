3ce49c058b48e07b27a0ef42c2876948831fdef0
Liudas <repkovas@gmail.com>
2022-03-14 18:06:58 +0200

    pamoka done

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useRef } from "react";
+import { StyleSheet, Text, View, Animated, Easing } from "react-native";
 export default function App() {
+  const kvadratuMasyvas = [];
+  const opacityValues = [];
+  const animations = [];
+
+  for (let i = 0; i < 500; i++) {
+    // kuriam skaicius
+    kvadratuMasyvas.push(i);
+    // kuriam animuotas vertes
+    opacityValues.push(new Animated.Value(0));
+    //kuriam pacias animacijas, kurios keis tas vertes per laika
+    animations.push(
+      Animated.timing(opacityValues[i], { toValue: 1, duration: 100 })
+    );
+  }
+
+  // kai puslapis pirma kart surenderinamas paleidziam animacijas
+  useEffect(() => {
+    Animated.stagger(5, animations).start()
+  }, []);
+
+  // animated.view kaip opacity nurodom issaugota reiksme
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {kvadratuMasyvas.map((skaicius, index) => {
+        return (
+          <Animated.View
+            key={index}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: "#F4743B",
+              marginLeft: 4,
+              marginTop: 4,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "white",
+              opacity: opacityValues[index],
+            }}
+          >
+          </Animated.View>
+        );
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +53,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
